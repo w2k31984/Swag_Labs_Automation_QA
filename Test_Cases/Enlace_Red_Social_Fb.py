119400f8e55d2f72d3a1770d1f48381485d36805
Cristian Parada <cmpmendoza12@gmail.com>
2021-06-03 15:19:46 -0600

    Creacion de Test Case Automation para validar enlace a red social FB de la empresa,Reporte de caso.

---
+++ Test_Cases/Enlace_Red_Social_Fb.py
@@ -0,0 +1,42 @@
+import time
+import unittest
+import HtmlTestRunner
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class myTestLinkSocialNetworkFb(unittest.TestCase):
+    def testLinkSocialNetworkFb(self):
+        driver = webdriver.Chrome(executable_path=r'C:\\Users\\cristian_parada\\Desktop\\Automatizacion_Test_Cases_Ecommerce\\chromedriver.exe')
+        driver.get('https://www.saucedemo.com/')
+        time.sleep(2)
+        usuario = driver.find_element_by_id('user-name')
+        usuario.clear()
+        usuario.send_keys('standard_user')
+
+        contrasena = driver.find_element_by_id('password')
+        contrasena.clear()
+        contrasena.send_keys('secret_sauce')
+        time.sleep(3)
+
+        driver.find_element_by_id('login-button').click()
+        time.sleep(2)
+#Haciendo scroll a la pagina e ir a enlaces de redes sociales
+        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+        time.sleep(5)
+#Dando click a enlaces de redes sociales.
+
+        # Haciendo click en icono de red social facebook y desplegando en ventana nueva en navegador
+        driver.find_element_by_class_name('social_facebook').click()
+        time.sleep(2)
+
+        #Regresando a la pagina de productos
+        driver.back()
+        time.sleep(5)
+
+        driver.stop_client()
+        driver.close()
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=r'C:\Users\cristian_parada\Desktop\Automatizacion_Test_Cases_Ecommerce\Reports'))
+
